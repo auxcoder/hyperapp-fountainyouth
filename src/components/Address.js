29808bf34eb07231d2345ffcde4f57856a32fd61
Pavel Duran <auxcoder@gmail.com>
2018-12-24 10:50:48 -0500

    Add Address component

---
+++ src/components/Address.js
@@ -0,0 +1,10 @@
+import { h } from 'hyperapp';
+
+export default () => (
+  <address className="">
+    330 SW 27 AVE Suite 502 Miami, FL 33135
+    <br />
+    <abbr title="Phone">P:</abbr> <a href="tel:(786) 717-6296">(786) 717-6296</a> &nbsp; &nbsp;
+    <abbr title="Email">E:</abbr> <a href="mailto:poncedeleon@fountaintouth4u.com">Support</a>
+  </address>
+);
